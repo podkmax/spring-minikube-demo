cdf47d98b109d2e16840720159e5ad95895336cb
Maksim.Podkorytov <maksim.podkorytov@x5.ru>
2025-07-31 02:52:55 +0300

    Ебани залупу

--- src/main/java/ru/javabox/deploydemo/HelloController.java
+++ src/main/java/ru/javabox/deploydemo/HelloController.java
@@ -9,4 +9,9 @@ public class HelloController {
     public String hello() {
         return "Hello, World!";
     }
+
+    @GetMapping("/zalupa")
+    public String zalupa() {
+        return "Zalupa Ivanycha!";
+    }
 }
